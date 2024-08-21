a1c91b2505958c93b3a1fa315a1954507a188652
Tyler Hall <0tillathehun0@gmail.com>
2024-08-21 20:12:35 +0000

    chore(repo): allow all assignables in process repl. More seamless gateway debugging

--- gateway.lua
+++ gateway.lua
@@ -1,6 +1,6 @@
 (function ()
   local gateway = require('.graphql.gateway.init')
 
-  local gql = gateway.create()
-  return gql
+  local gql, apis = gateway.create({ continue = true })
+  return gql, apis
 end)()
--- repl.js
+++ repl.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'node:url'
 import readline from 'node:readline'
 import { createReadStream, readFileSync } from 'node:fs'
 import { Readable } from 'node:stream'
+import { randomInt, randomUUID } from 'node:crypto'
 
 import chalk from 'chalk'
 import AoLoader from '@permaweb/ao-loader'
@@ -27,11 +28,35 @@ function wasmResponse (stream) {
   return new Response(stream, { headers: { 'Content-Type': 'application/wasm' } })
 }
 
-async function replWith ({ stream, env }) {
+async function replWith ({ ASSIGNABLE, stream, env }) {
+  let messageCount = 0
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   })
+
+  async function maybeAllowAssignments (init) {
+    if (!ASSIGNABLE) return init
+
+    console.log(chalk.gray(`Adding assignable: '${ASSIGNABLE}'`))
+    const allowAllAssignmentsMessage = {
+      Id: 'allow-assignments',
+      Target: env.Process.Id,
+      From: env.Process.Owner,
+      Owner: env.Process.Owner,
+      'Block-Height': randomInt(1_000_000, 1_500_000),
+      Module: env.Module.Id,
+      Tags: [
+        { name: 'Action', value: 'Eval' }
+      ],
+      Data: `
+        ao.addAssignable('assignable', ${ASSIGNABLE})
+      `
+    }
+
+    const { Memory } = await handle(init, allowAllAssignmentsMessage, env)
+    return Memory
+  }
 
   function createEval (line) {
     if (line === 'sample-gql') {
@@ -49,23 +74,27 @@ ao.server('query GetPersons { persons { firstName, lastName } }')
       console.log(chalk.green(`
 Example:
 
-ao.server('query Introspection { __schema { types { name } } }')
+ao.server('query { transaction (id: "540a3be5-dca2-47bf-97bc-27c754acc945") { id } }')
 `))
       const init = readFileSync(join(__dirname, 'gateway.lua'), 'utf-8')
-      line = `ao.server = ao.server or ${init}`
+      line = `local gql, apis = ${init}; ao.server = gql; ao.apis = apis;`
     } else if (line.startsWith('query') || line.startsWith('mutation')) {
       line = `return ao.server('${line}')`
     }
 
+    const id = randomUUID()
+    console.log(`Sending msg: ${chalk.bgBlue(id)}`)
+
     return {
-      Id: 'message',
+      Id: id,
       Target: env.Process.Id,
       From: env.Process.Owner,
       Owner: env.Process.Owner,
-      'Block-Height': '1000',
+      'Block-Height': randomInt(1_000_000, 1_500_000),
       Module: env.Module.Id,
       Tags: [
-        { name: 'Action', value: 'Eval' }
+        { name: 'Action', value: 'Eval' },
+        { name: 'Message-Count', value: ++messageCount }
       ],
       Data: line
     }
@@ -104,23 +133,29 @@ ao.server('query Introspection { __schema { types { name } } }')
     )
   )
 
-  return (init) => trampoline(() => repl(init))
+  return (init) => Promise.resolve(init)
+    .then(maybeAllowAssignments)
+    .then((init) => trampoline(() => repl(init)))
     .finally(() => rl.close())
 }
 
 replWith({
+  /**
+   * Assignable that allows all assignments
+   */
+  ASSIGNABLE: 'function (msg) return true end',
   stream: await binaryStream(!!process.env.USE_AOS),
   env: {
     Process: {
       Id: 'PROCESS_TEST',
-      Owner: 'OWNER',
+      Owner: randomUUID(),
       Tags: [
         { name: 'Module', value: 'aos-graphql' }
       ]
     },
     Module: {
       Id: 'MODULE_TEST',
-      Owner: 'OWNER',
+      Owner: randomUUID(),
       Tags: []
     }
   }
